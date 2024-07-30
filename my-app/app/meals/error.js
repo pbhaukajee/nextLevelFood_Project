fbac13b9d244516fddee62e27aa798231ce67549
Priyanka <pbhaukajee@gmail.com>
2024-07-30 14:35:33 -0500

    add meal details

--- my-app/app/meals/error.js
+++ my-app/app/meals/error.js
@@ -4,7 +4,7 @@ export default function Error({ error }) {
   return (
     <main className="error">
       <h1>An error occured!</h1>
-      <p>Faild to fetch meal data. Please try again later.</p>
+      <p>Failed to fetch meal data. Please try again later.</p>
     </main>
   );
 }
